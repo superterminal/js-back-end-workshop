4b5f37a5bdda82e3cfa849b719d5d42637542db4
superfan <trendemil@gmail.com>
2019-10-13 14:46:02 +0300

    updated routes

--- config/express.js
+++ config/express.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
 const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const secret = 'secret';
 
 module.exports = (app) => {
    
-    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(express.urlencoded({extended: false}));
+    app.use(cookieParser(secret));
     app.use(express.static(path.resolve(__basedir, 'static')));
     app.engine('.hbs', handlebars({extname: '.hbs', defaultLayout: false}));
     app.set('views', path.resolve(__basedir, 'views'));
