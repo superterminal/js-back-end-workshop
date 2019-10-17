552848f3ab585a6fafff9ea3d3c0734b286da292
superfan <trendemil@gmail.com>
2019-10-17 19:49:33 +0300

    updated cookie expiring time to 20m

--- utils/jwt.js
+++ utils/jwt.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const secret = 'shhhhh';
 
 function createToken(data) {
-    return jwt.sign(data, secret, { expiresIn: '10m'});
+    return jwt.sign(data, secret, { expiresIn: '20m'});
 }
 
 function verifyToken(token) {
