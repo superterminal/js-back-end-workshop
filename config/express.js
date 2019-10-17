ab9fc213b2754f17004a8946b31bf16d0f15c3a5
superfan <trendemil@gmail.com>
2019-10-17 19:35:52 +0300

    added path to partials in express

--- config/express.js
+++ config/express.js
@@ -10,7 +10,13 @@ module.exports = (app) => {
     app.use(express.urlencoded({extended: false}));
     app.use(cookieParser(secret));
     app.use(express.static(path.resolve(__basedir, 'static')));
-    app.engine('.hbs', handlebars({extname: '.hbs', defaultLayout: false}));
+    app.engine('.hbs', handlebars(
+        {
+            extname: '.hbs', 
+            defaultLayout: false,
+            partialsDir: __basedir + '/views/partials'
+        }
+    ));
     app.set('views', path.resolve(__basedir, 'views'));
 
 };
