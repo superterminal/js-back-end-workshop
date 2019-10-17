c8888ec7503a82fb06d29c791ae526886e09c3d3
superfan <trendemil@gmail.com>
2019-10-17 19:49:59 +0300

    added cube name validation in model

--- models/cube.js
+++ models/cube.js
@@ -1,7 +1,24 @@
 const mongoose = require('mongoose');
 
 const cubeSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true,
+        validate: [
+            {
+                validator: v => {
+                    return v.length > 2;
+                },
+                message: props => `${props.value} length should be larger than 2!`
+            },
+            {
+                validator: v => {
+                    return v.length < 10;
+                },
+                message: props => `${props.value} length should be less than 10!`
+            }
+        ]
+    },
     description: String,
     imageUrl: String,
     difficultyLevel: Number,
